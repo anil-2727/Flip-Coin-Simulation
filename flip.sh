72bfb56affb9562fec7e9806d518655d5ba48190
anil-2727 <bali39959@gmail.com>
2021-06-06 00:21:34 +0530

    Added:21times

--- flip.sh
+++ flip.sh
@@ -4,18 +4,26 @@ loop=0
 heads=0
 tails=0
 
-while [ loop -le 10 ]
+while [[ $heads -le 21 ]] && [[ $tails -le 21 ]];
 do
+
 x=$((RANDOM%2))
-if [ $x -eq 0 ]
+if [[ $x -eq 0 ]];
 then
-echo "winner : heads"
+echo winner : heads
 ((heads++))
 else 
-echo "winner : tails"
+echo winner : tails
 ((tails++))
 fi
-((loop++))
 done
-echo "heads :$heads"
-echo "tails :$tails"
+echo heads :$heads
+echo tails :$tails
+
+if [[ $heads -gt $tails ]];then
+echo heads won :"$heads"matches
+elif [[ $heads -eq $tails ]];then
+echo its tie   :"$heads"heads & "$tails"tails
+else
+echo "tails won :$tails matches"
+fi
